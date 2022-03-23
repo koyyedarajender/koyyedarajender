5fe4adf1a61d55788bf96785cdcdbfab4486dee2
koyyedarajender <koyyedarajender@gmail.com>
2022-03-23 21:44:41 +0530

    FibonacciSeriesPractice

---
+++ src/test/java/com/org/project/practicePackage/FibonacciSeriesPractice.java
@@ -0,0 +1,19 @@
+package com.org.project.practicePackage;
+
+public class FibonacciSeriesPractice {
+    public static void main(String[]args){
+        int a = 0;
+        int b = 1;
+        int sum;
+        System.out.println(+a);
+        System.out.println(+b);
+        for(int i=1;i<=10;i++) {
+             sum = a + b;
+            System.out.println("" + sum);
+            a = b;
+            b = sum;
+         }
+
+
+    }
+}
