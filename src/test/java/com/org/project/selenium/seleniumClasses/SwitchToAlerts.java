33eb24f64296cb7915cb71daac0c07f8774b17de
Rajender koyyeda <rkoyyeda@otalio.com>
2022-04-22 20:21:54 +0530

    test

---
+++ src/test/java/com/org/project/selenium/seleniumClasses/SwitchToAlerts.java
@@ -0,0 +1,53 @@
+package com.org.project.selenium.seleniumClasses;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.awt.*;
+
+/**
+ * Created by rajender.koyyeda on 22-04-2022.
+ */
+public class SwitchToAlerts {
+
+    public static void main(String args[]) throws InterruptedException, AWTException {
+
+        String path = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver", path + "\\src\\test\\resources\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+
+       // driver.get("https://demo.guru99.com/test/delete_customer.php");
+
+        driver.get("https://www.facebook.com/");
+
+        Thread.sleep(2000);
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+
+        //active element code
+
+        System.out.println("the active element  is "+driver.switchTo().activeElement().getText());
+        driver.switchTo().activeElement().sendKeys("Rajender");
+        driver.switchTo().activeElement().clear();
+        Thread.sleep(2000);
+        driver.switchTo().activeElement().sendKeys("Rajender");
+        Thread.sleep(2000);
+        /*driver.findElement(By.name("cusid")).sendKeys("ABCD");
+        driver.findElement(By.name("submit")).click();
+        Thread.sleep(2000);
+
+        System.out.println("the alert message is "+driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();
+        Thread.sleep(2000);
+        driver.findElement(By.name("submit")).click();
+        Thread.sleep(2000);
+        driver.switchTo().alert().accept();
+        System.out.println("the 2nd alert message is "+driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();*/
+        driver.quit();
+
+    }
+}
--- src/test/java/com/org/project/selenium/seleniumClasses/WebDriverBasicMethods.java
+++ src/test/java/com/org/project/selenium/seleniumClasses/WebDriverBasicMethods.java
@@ -1,15 +1,24 @@
 package com.org.project.selenium.seleniumClasses;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by rajender.koyyeda on 18-04-2022.
  */
 public class WebDriverBasicMethods {
 
-    public static void main(String args[]) throws InterruptedException {
+    public static void main(String args[]) throws InterruptedException, AWTException {
 
         String path = System.getProperty("user.dir");
         System.setProperty("webdriver.chrome.driver", path + "\\src\\test\\resources\\drivers\\chromedriver.exe");
@@ -21,20 +30,15 @@ public class WebDriverBasicMethods {
         driver.manage().window().maximize();
         Thread.sleep(2000);
         //driver.close();
-        System.out.println("the title of website is "+ driver.getTitle());
-
+        /*System.out.println("the title of website is "+ driver.getTitle());
         System.out.println("the page source of website is "+ driver.getPageSource());
-
-
         String Curl= driver.getCurrentUrl();
-
         System.out.println("the current URL  of website is "+ Curl);
-
         if(Curl.contains("https")){
             System.out.println("the current URL is secure");
         }else{
             System.out.println("the current URL is not secure");
-        }
+        }*/
 
 
 
@@ -42,26 +46,52 @@ public class WebDriverBasicMethods {
 
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         Thread.sleep(2000);
-        driver.findElement(By.id("user-name")).clear();
+
+        /*driver.findElement(By.id("user-name")).clear();
         Thread.sleep(2000);
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        Thread.sleep(2000);
-
+        Thread.sleep(2000);*/
 
 
         driver.findElement(By.name("password")).sendKeys(password);
         Thread.sleep(2000);
-        System.out.println("the getattribute value for login button is "+driver.findElement(By.id("login-button")).getAttribute("type"));
-
+        /*System.out.println("the getattribute value for login button is "+driver.findElement(By.id("login-button")).getAttribute("type"));
         System.out.println("the getcss color value for login button is "+driver.findElement(By.id("login-button")).getCssValue("border"));
+*/
 
+        String name = driver.findElement(By.id("login-button")).getAttribute("value");
+
+        Assert.assertEquals(name, "Login");
+        Assert.assertTrue(name.contains("Login"));
+
+        /*SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(name, "rajender");*/
+
+        System.out.println("the login element name validation success");
+        //driver.switchTo().frame("name");
 
         driver.findElement(By.id("login-button")).click();
         System.out.println("the login success");
         Thread.sleep(3000);
 
-        String p1= driver.findElement(By.xpath("(//div[@class='inventory_item_name'])[1]")).getText();
-        System.out.println("the 1st product name is "+ p1);
+        /*driver.navigate().to("https://www.saucedemo.com/");
+        Thread.sleep(3000);
+        driver.get("https://www.saucedemo.com/");
+        Thread.sleep(3000);*/
+
+      //  ---->
+
+
+        String parent=driver.getWindowHandle();
+        System.out.println("the current window handle name is :" +parent);
+
+
+
+
+        /*String p1= driver.findElement(By.xpath("(//div[@class='inventory_item_name'])[1]")).getText();
+        System.out.println("the 1st product name is "+ p1);*/
+
+
         driver.quit();
 
     }
