83cfb64ea7e682f37417dc94680410c15f6dc3e2
Lenovo <koyyedarajender@gmail.com>
2025-01-11 09:25:43 +0530

    Test class updated

--- src/test/java/com/project/application/KT/seleniumPractiesClasses/Test.java
+++ src/test/java/com/project/application/KT/seleniumPractiesClasses/Test.java
@@ -1,11 +1,9 @@
-package com.project.application.KT.selenium.seleniumPractiesClasses;
+package com.project.application.KT.seleniumPractiesClasses;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import static com.project.application.utills.ConfigFile.projectDirectory;
 
 /**
  * Created by rajender.koyyeda on 19-04-2022.
@@ -34,22 +32,43 @@ public class Test {
 
         WebDriver driver = new ChromeDriver(chromeOptions);
 
-        driver.get("https://www.saucedemo.com/");
 
+
+        driver.get("https://www.saucedemo.com/");
+        Thread.sleep(1000);
 
 
 
         driver.findElement(By.id("user-name")).click();
-        System.out.println("clicked on user name");
 
+
+       /* System.out.println("clicked on user name");
         driver.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div/div/form/div[1]/input")).click();
         System.out.println("clicked on user name2");
         driver.findElement(By.xpath("//input[@id='user-name']")).click();
-        System.out.println("clicked on user name3");
-        //input[@id='user-name']
-        Thread.sleep(2000);
+        System.out.println("clicked on user name3");*/
 
-        driver.close();
+
+
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+        Thread.sleep(1000);
+
+        String username= driver.findElement(By.xpath("//input[@id='user-name']")).getAttribute("value");
+        System.out.println("My logged in user is: "+username);
+
+        String logo= driver.findElement(By.xpath("//div[@class=\"login_logo\"]")).getText();
+        System.out.println("My logo: "+logo);
+
+        String usercolor= driver.findElement(By.xpath("//input[@id='user-name']")).getCssValue("color");
+        System.out.println("My logged in user color  is: "+usercolor);
+
+
+
+        //input[@id='user-name']
+
+        Thread.sleep(1000);
+
+        // driver.close();
         System.out.println("closed");
         driver.quit();
 
